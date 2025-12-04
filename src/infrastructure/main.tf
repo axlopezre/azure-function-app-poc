085c98e1ce3e283fd91953a13ac8603c2af3ff7b
Axel <axeldalli@gmail.com>
2025-12-04 11:17:42 -0600

    frontend terraform configuration

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -1,7 +1,8 @@
 locals {
   rg_name          = "${var.project_name}-rg"
   storage_name     = replace(lower("${var.project_name}stg"), "-", "")
-  plan_name        = "${var.project_name}-plan"
+  backend_plan_name        = "${var.project_name}-backend-plan"
+  frontend_plan_name = "${var.project_name}-frontend-plan"  # plan del frontend
   functionapp_name = "${var.project_name}-func"
 }
 
@@ -33,7 +34,7 @@ resource "azurerm_storage_account" "functions" {
 
 # Consumption plan para Functions
 resource "azurerm_service_plan" "plan" {
-  name                = local.plan_name
+  name                = local.backend_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -74,12 +75,22 @@ output "function_app_name" {
   value       = azurerm_linux_function_app.functions.name
 }
 
+# App Service Plan para el frontend (Next.js)
+resource "azurerm_service_plan" "frontend_plan" {
+  name                = local.frontend_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku_name = "B1"   # o "F1" (Free), "S1", etc. según lo que te permita la subscripción
+  os_type  = "Linux"
+}
+
 # Web App Linux para Next.js
 resource "azurerm_linux_web_app" "frontend" {
   name                = var.frontend_app_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.frontend_plan.id
 
   https_only = true
 
