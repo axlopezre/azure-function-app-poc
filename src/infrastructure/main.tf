e4cdc21ea3808fb7a79f3d4568b811d77711661f
Axel <axeldalli@gmail.com>
2025-12-04 19:51:22 -0600

    infra changes

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -3,8 +3,16 @@ locals {
   storage_name      = replace(lower("${var.project_name}stg"), "-", "")
   env_suffix_full   = "-${lower(var.environment)}"
   backend_plan_name = "${var.project_name}-backend-plan"
-  function_app_name = trim("${var.project_name}-func${local.env_suffix_full}", " ")
-  frontend_app_name = trim("${var.project_name}-frontend${local.env_suffix_full}", " ")
+  function_app_name = regexreplace(
+    lower(trim("${var.project_name}-func${local.env_suffix_full}", " ")),
+    "[^0-9a-z-]",
+    "-"
+  )
+  frontend_app_name = regexreplace(
+    lower(trim("${var.project_name}-frontend${local.env_suffix_full}", " ")),
+    "[^0-9a-z-]",
+    "-"
+  )
 }
 
 # Resource Group
