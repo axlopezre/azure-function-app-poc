859628ec57861d012d11eff25d75b36e723b9c56
Axel <axeldalli@gmail.com>
2025-12-04 19:37:48 -0600

    front end test changes

--- src/Frontend/app/page.tsx
+++ src/Frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="flex justify-center items-center flex-col">
-      <h1 className="font-sans font-bold">Título en Montserrat TEST AXEL 3</h1>
+      <h1 className="font-sans font-bold">Título en Montserrat TEST AXEL 4</h1>
       <p className="font-lato text-base">El mejor software de facturas</p>
     </div>
   );
